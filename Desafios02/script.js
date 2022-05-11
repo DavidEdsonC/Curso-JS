9174fa4fa30f5caf228c50bfbb3e0dce31ddc06b
David Edson C <dedson82@gmail.com>
2022-05-11 05:53:07 -0300

    Ajustes

--- Desafios02/script.js
+++ Desafios02/script.js
@@ -10,40 +10,40 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        img.getAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
-                img.setAttribute('src', 'menino1.png')
+                img.getAttribute('src', 'menino1.png')
             } else if (idade < 21) {
                 // Jovem
-                img.setAttribute('src', 'jovem1.png')
+                img.getAttribute('src', 'jovem1.png')
             } else if (idade < 50) {
                 // adulto
-                img.setAttribute('src', 'homem1.png')
+                img.getAttribute('src', 'homem1.png')
             } else {
                 // Idoso
-                img.setAttribute('src', 'idoso1.png')
+                img.getAttribute('src', 'idoso1.png')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
-                img.setAttribute('src', 'menina1.png')
+                img.getAttribute('src', 'menina1.png')
             } else if (idade < 21) {
                 // Jovem
-                img.setAttribute('src', 'jovem2.png')
+                img.getAttribute('src', 'jovem2.png')
             } else if (idade < 50) {
                 // adulto
                 img.setAttribute('src', 'mulher1.png')
             } else {
                 // Idoso
-                img.setAttribute('src', 'idosa1.png')
+                img.getAttribute('src', 'idosa1.png')
             }
         }
-        res.style.textAling = 'center'
-        res.innerHTML = 'Detectamos ${gênero} com ${idade} anos. '
+        
+        res.innerHTML = ('Detectamos ${gênero} com ${idade} anos. ')
         res.appendChild(img)
     }
 }
