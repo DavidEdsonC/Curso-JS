2c154e9c05a55adf1f79faca0d8a95019f6920cd
David Edson C <dedson82@gmail.com>
2022-05-15 04:25:45 -0300

    Estudo de array

--- Desafio03/ex16/laço-fot.js
+++ Desafio03/ex16/laço-fot.js
@@ -11,6 +11,10 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if ( p <= 0){
+            window.alert('Pré-encha o Campo de [PASSOS] minimo de PASSOS 1')
+            p = 1
+        }
         if (i < f) {
             // Contagem crescente
             for (let c = i; c <= f; c += p) {
