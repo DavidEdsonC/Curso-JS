4c4c0c5802f5e8bca761b4b8069b452307b09703
David Edson C <dedson82@gmail.com>
2022-05-17 00:48:20 -0300

    Desafio Finalizado

--- Desafio04/Modelo/ex18.js
+++ Desafio04/Modelo/ex18.js
@@ -21,8 +21,39 @@ function inLista(n, l){
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert('Tudo Ok!')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
     }else {
         window.alert('Valor inválidoou não encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function fim() {
+    if (valores.length == 0) {
+        window,alert('Adicione um valor antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</P>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</P>`
+        res.innerHTML += `<p>A soma de todos os valores, é ${soma}.</P>`
+        res.innerHTML += `<p>A médeia dos valores digitados é ${media}.</p>`
     }
 }
